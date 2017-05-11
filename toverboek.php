a66ced48cecebc7931d32e6ca42af5f8029a873d
Erwinoostland <erwinoostland1@hotmail.com>
2017-05-11 14:07:02 +0200

    Add files via upload

---
+++ toverboek.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="css/toverboek.css">
+</head>
+<body>
+<!--NAVIGATIE BALK PLUS HEADER-->
+<section class="titelvak">
+    <img class="titellogo1" src="img/Bezem2.png">
+    <p class="titel">DE VLIEGENDE BEZEM</p>
+    <img class="titellogo2" src="img/Ketel1.png">
+</section>
+
+<nav class="navbar">
+    <ul>
+        <li class=""><a href="index.php">HOME</a></li>
+        <li><a href="shop.php">SHOP</a></li>
+        <li><a href="toverboek.php">TOVERBOEK</a></li>
+        <li><a href="heksen.php">BEROEMDE HEKSEN</a></li>
+    </ul>
+</nav>
+<!--Spreuken-->
+<section class="wijsheid">
+    <h1>Je wijsheid vergroten</h1>
+    <p>Salie staat al sinds mensenheugenis bekend als een magisch kruid.<br>
+    De Native Americans gebruikten al salie voor het reinigen van hun heilige plaatsen.<br>
+    De Oude Kelten dachten dat een bruid wijs zou worden als ze een buideltje met salie bij zich droeg.</p>
+<ul class="saliespreuk">
+    <li class="salieli">In magische cirkels die men overal vindt,</li><br>
+    bij hevige storm of rustige wind,<br>
+    wanneer de magie slaapt of waakt..<br>
+    het is in de cirkel dat wijsheid je raakt.<br>
+</ul>
+</section>
+<section class="salieimg">
+    <img src="img/salieblad.png" alt="Salie blad">
+</section>
+
+<section class="munt">
+    <h1>Ergens munt uit slaan</h1>
+    <p>Door het kruid munt te gebruiken kun je een beetje geld naar je toe trekken.<br>
+        Dit werkt alleen als je het écht nodig hebt. Maak een zakje van een stukje groen vilt en doe er 4 gedroogde muntblaadjes in.<br>
+        Neem nu een stukje hemelsblauw papier en schrijf daarop het bedrag wat je nodig hebt.<br>
+        Leg het zakje met de muntblaadjes en het papier in het midden van een tafeltje, of van je altaar.<br>
+        Zet in elke hoek een brandende, goudkleurige kaars en leg een muntstukje bij de 4 kaarsen.<br>
+        De volgende spreuk wordt gezongen terwijl je 4x rond de tafel danst:<br></p>
+    <ul class="muntspreuk">
+        <li class="muntli">Meester van de munt, is mij wat geld gegund?<br>
+            Ik zal hard werken voor het bedrag en ik hoop dat ik er snel over beschikken mag.<br>
+            Ik zal alles terugverdienen, tot de laatste cent, ik hoop dat u vrijgevig bent.<br>
+    </ul>
+</section>
+<section class="muntimg">
+    <img src="img/munt.png" alt="Salie blad" height="130" width="130">
+</section>
+<br>
+<br>
+<br>
+<br>
+<section class="maanritueel">
+    <h1>Een maanritueel om je in contact te brengen met je onderbewuste</h1>
+    <p>Het wassen en afnemen van de maan beïnvloedt onze dromen en bevordert onze intuïtie.<br>
+        Dit ritueel moet je uitvoeren in een heldere, wassende door de maan verlichte nacht.<br>
+        Je legt een klein boeketje witte bloemen (liefst ook met een takje jasmijn) en een rond spiegeltje in het maanlicht op je vensterbank.<br>
+        Ga bij de vensterbank staan, kijk naar de maan en spreek deze spreuk uit:<br></p>
+    <ul class="maanspreuk">
+    <li class="maanli">O maan, vriend van fonkelende sterren, of wie u ook zijn mag.</li><br>
+    Uw zilveren stralen volgen het vurige licht van de dag.<br>
+    O maan met de vele gezichten, u kent mijn dromen.<br>
+    Help mij en laat mijn binnenste naar boven komen.<br>
+    </ul>
+</section>
+<section class="maanimg">
+    <img src="img/maanritueel.jpg" alt="Salie blad" height="130" width="180">
+</section>
+</body>
+</html>
