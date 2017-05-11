45105ed33587ccd3c469500e2bbab7391cf31946
Jasperschuiling <jasperschuiling@gmail.com>
2017-05-11 14:08:39 +0200

    Add files via upload

---
+++ shop.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>De vliegende Bezem Shop</title>
+    <link rel="stylesheet" href="css/shop.css">
+</head>
+<body>
+
+<section class="titelvak">
+    <img class="titellogo1" src="img/speelgoedbezem.png">
+    <p class="titel">DE VLIEGENDE BEZEM</p>
+    <img class="titellogo2" src="img/heksenketel1.png">
+</section>
+
+<nav class="navbar">
+    <ul>
+        <li class=""><a href="index.php">HOME</a></li>
+        <li><a href="shop.php">SHOP</a></li>
+        <li><a href="toverboek.php">TOVERBOEK</a></li>
+        <li><a href="heksen.php">BEROEMDE HEKSEN</a></li>
+    </ul>
+</nav>
+<!-- vak waar de welkomstekst van de shop in staat -->
+
+<section class="shoptekstvak">
+    <strong class="shoptekstvakheading">Shop</strong>
+    <p class="shoptekstvaktxt">Hallo en welkom bij de shop van de Vliegende Bezem. Hier kan u onze ruime selectie aan bezems en ketels kopen <br>
+    De bezems zijn uitvoerig getest door onze werknemers. En de ketels zijn allemaal gemaakt van gegoten staal dat geforceerd is met chroom. </p>
+</section>
+
+<!-- Vak waar de ketels in staan -->
+
+<section class = ketels>
+    <h1 class="h1Ketels">Ketels</h1>
+
+    <!-- 1ste ketel -->
+
+    <article class="ketel1vak">
+        <img class="ketel1" src="img/ketel1.png">
+        <p class="ketel1txt"><h1>Beginner Ketel</h1><br>Ijzeren ketel. Goed voor beginnende heksen. <br> Hier kan u uw experimenten in brouwen,
+        <br> tegen een goed prijsje.<br><br><br><strong>€49,99</strong>  <button>  Bestellen.</button></p>
+    </article>
+
+    <!-- 2de ketel -->
+    <article class="ketel2vak">
+        <img class="ketel2" src="img/ketel2.png">
+        <p class="ketel2txt"><h1>Geadvanceerde Ketel</h1><br>Dit is een stalen ketel.<br>Geforceerd met chroom.<br>Hier kan u zelf soep in koken!
+        <br><br><br><strong>€109.99</strong>   <button>Bestellen.</button></p>
+    </article>
+
+    <!-- 3de ketel -->
+    <article class="ketelvak3">
+        <img class="ketel3" src="img/ketel3.png">
+        <p class="ketel3txt"><h1>Pro Ketel</h1><br>Dit is de beste ketel die u kan krijgen.<br> U kan hier vanalles in brouwen.
+        <br>Van alchemische recepten tot aan kinderen. <br>Het wil allemaal!<br><br><br><strong>€199,99</strong>   <button>Bestellen.</button> </p>
+    </article>
+
+
+
+</section>
+
+<section class="bezems">
+    <h1 class="h1Bezems">Bezems</h1>
+
+    <article class="bezem1vak">
+        <img class="bezem1" src="img/bezem1.png">
+        <p class="bezem1txt"><h1>Standaard Bezem</h1><br>Dit is de standaard bezem die u hier kan kopen.<br>
+        Deze bezem kan op normale snelheid vliegen,<br> Tot een hoogte van 200 meter</p><br><br><br><strong>25,99</strong>   <button>Bestellen.</button>
+
+    </article>
+
+    <article class="bezem2vak">
+        <img class="bezem2" src="img/bezem2.png">
+        <p class="bezem2txt"><h1>Ultra Bezem</h1><br>Met deze bezem kan u uzelf een echte heks noemen.<br>
+        Deze bezem vliegt als een malle. <br>Met een snelheid van 100 km/h en op een<br> hoogte van 600 meter<br> kan deze bezem vandaag van u zijn</p>
+        <br><strong>239,99</strong>   <button>Bestellen.</button>
+    </article>
+
+    <article class="bezem3vak">
+        <img class="bezem3" src="img/bezem3.png">
+        <p class="bezem3txt"><h1>Nimbus 2000</h1><br>Dit is de Nimbus 2000. Dit is de beste bezem ter wereld.<br> U gaat met 300 km/h door de lucht heen.<br>
+        Op een hoogte van 1000 meter.<br> Een must have voor elke pro heks!<br><br><br><strong>4990,99</strong>   <button>Bestellen.</button></p>
+    </article>
+</section>
+
+</body>
+</html>
