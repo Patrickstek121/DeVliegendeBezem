86c56782158b25a7a286de41cb135f4578f3fd07
Patrickstek121 <p.stek@ziggo.nl>
2017-05-11 13:45:22 +0200

    Add files via upload

---
+++ heksen.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="refresh" content="5"/>
+    <link rel="stylesheet" type="text/css" href="css/heksen.css">
+    <title>De vliegende bezem</title>
+</head>
+
+<body>
+
+<section class="titelvak">
+    <img class="titellogo1" src="img/speelgoedbezem.png">
+    <p class="titel">DE VLIEGENDE BEZEM</p>
+    <img class="titellogo2" src="img/heksenketel1.png">
+</section>
+
+<nav class="navbar">
+    <ul>
+        <li class=""><a href="index.php">HOME</a></li>
+        <li><a href="shop.php">SHOP</a></li>
+        <li><a href="toverboek.php">TOVERBOEK</a></li>
+        <li><a href="heksen.php">BEROEMDE HEKSEN</a></li>
+    </ul>
+</nav>
+<section class="afbeelding">
+    <img src="img/heks1.jpg">
+    <br><p class="tekst1"> Alice Kyteler was waarschijnlijk de eerste die veroordeeld is voor hekserij.
+    Ze was een machtige en rijke vrouw, geboren in 1280, en ze was maarliefst vier keer getrouwd.
+    Alle echtgenoten waren kort na het huwelijk gestorven.
+    Toen de vierde echtgenoot vreemde ziekteverschijnselen begon te vertonen klaagde hij haar aan voor hekserij.
+        </p>
+    <br>
+    <img src="img/heks2.jpg">
+    <br><p class="tekst2"> Katharina was de moeder van de beroemde astroloog Johannes Kepler.
+        Ze is geboren in 1546. Door een ruzie met haar buurvrouw, Ursula Reinbold, werd Katharina door de broer van Usula bedreigd en beschuldigd van hekserij bij de schout.
+        Het proces werd in gang gezet en er werden allerlei bewijzen tevoorschijn gehaald:
+        Katharina zou mensen hebben betoverd en mensen in haar omgeving waren ziek geworden of zelfs gestorven.
+    </p>
+    <br>
+    <img src="img/heks3.jpg">
+    <br><p class="tekst3">
+    Jeanne werd in 1412 geboren in een dorpje in Frankrijk, ten tijde van de Honderdjarige Oorlog tussen Frankrijk en Engeland.
+        In haar kindertijd begon ze stemmen te horen die gepaard gingen met een helder licht.
+        Ook zag ze visioenen en sprak ze met aartsengel Michaël.
+        In 1429 meldde Jeanne zich bij de Dauphin van Frankrijk als redder, gestuurd door God.
+        Ze beweerde dat ze hem kon helpen zijn koninkrijk terug te winnen van de Engelsen, en het beleg van Orléans werd verbroken.
+    </p>
+    <img src="img/heks4.jpg">
+    <br><p class="tekst4">
+    Gilles was één van de medestrijders van Jeanne d’Arc. Hij stond bekend als een waardig ridder en werd zelfs benoemd tot ‘Maarschalk van Frankrijk’.
+        Hij was de rijkste edelman van Europa totdat hij zijn gehele vermogen verspeelde.
+        Gilles begon zich op magie, alchemie en exorcisme te storten.
+        Een aantal jaren na de executie van Jeanne beschuldigde de staat hem ervan bezeten te zijn van de duivel en onder satanische invloed honderdveertig kinderen te hebben vermoord.
+    </p>
+</section>
+</section>
+</body>
+</html>
